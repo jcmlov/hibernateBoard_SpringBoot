03f3778f678aa90d15cee5a8bb9f0e64684f17ae
jcmlov@gmail.com <jcmlov@gmail.com>
2018-01-11 18:51:05 +0900

    init commit

---
+++ src/main/java/org/hibernateBoard/HibernateBoardApplication.java
@@ -0,0 +1,12 @@
+package org.hibernateBoard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateBoardApplication.class, args);
+	}
+}
